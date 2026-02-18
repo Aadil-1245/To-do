b0ac5066a1867e9f087e57a28b7a540676475fda
Aadil Sherfudin <aadil.sherfudin@illuminei.com>
2026-02-18 17:47:14 +0530

    Updates

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import KanbanBoard from './pages/KanbanBoard'
+import PrivateRoute from './components/PrivateRoute'
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/board/:projectId" element={<PrivateRoute><KanbanBoard /></PrivateRoute>} />
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
+
+export default App
