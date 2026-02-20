ecd03dfcf4e51345b5a2f6ea654e12f5475e7ebf
Aadil Sherfudin <aadil.sherfudin@illuminei.com>
2026-02-20 17:09:23 +0530

    changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174"],
+    allow_origins=[""],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
