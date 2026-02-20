ef46662e3897d2a3dca8c4508a01f3ce66ed5aa6
Aadil S <135581341+Aadil-1245@users.noreply.github.com>
2026-02-20 17:14:23 +0530

    Update CORS middleware to allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
